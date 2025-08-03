d5226a6210a7f2389cd587102977e3cb0915c315
nullptr <lia@bertran.xyz>
2025-08-03 22:46:03 +0200

    feat: Created `FlashbangDeploy` command and added `light_theme` config

--- lua/flashbang/config.lua
+++ lua/flashbang/config.lua
@@ -9,6 +9,7 @@ Flashbang.options = {
     min_interval = 5,
     max_interval = 20,
     duration = 2.5,
+    light_theme = "delek",
 }
 
 ---@private
--- lua/flashbang/init.lua
+++ lua/flashbang/init.lua
@@ -3,39 +3,43 @@ local sound = require("flashbang.sound")
 
 local Flashbang = {}
 
+local function deploy()
+    local duration = _G.Flashbang.config.duration * 1000
+    sound.play("flashbang")
+    local timer = vim.uv.new_timer()
+    local current = vim.g.colors_name
+    timer:start(
+        1300,
+        0,
+        vim.schedule_wrap(function()
+            vim.cmd("colorscheme " .. _G.Flashbang.config.light_theme)
+            vim.cmd("set background=light")
+            timer:start(
+                duration,
+                0,
+                vim.schedule_wrap(function()
+                    vim.cmd("colorscheme " .. current)
+                    vim.cmd("set background=dark")
+                end)
+            )
+        end)
+    )
+end
+Flashbang.deploy = deploy
+
 function Flashbang.setup(opts)
     _G.Flashbang.config = config.setup(opts)
+
+    vim.api.nvim_create_user_command("FlashbangDeploy", function()
+        deploy()
+    end, { desc = "Deploy a flashbang" })
 
     sound.detect_provider()
 
     local min_interval = _G.Flashbang.config.min_interval * 1000 * 60
     local max_interval = _G.Flashbang.config.max_interval * 1000 * 60
-    local duration = _G.Flashbang.config.duration * 1000
 
-    local function deploy()
-        sound.play("flashbang")
-        local timer = vim.loop.new_timer()
-        local current = vim.g.colors_name
-        timer:start(
-            1300,
-            0,
-            vim.schedule_wrap(function()
-                vim.cmd("colorscheme delek")
-                vim.cmd("set background=light")
-                timer:start(
-                    duration,
-                    0,
-                    vim.schedule_wrap(function()
-                        vim.cmd("colorscheme " .. current)
-                        vim.cmd("set background=dark")
-                    end)
-                )
-            end)
-        )
-    end
-    -- deploy()
-
-    local timer = vim.loop.new_timer()
+    local timer = vim.uv.new_timer()
     local function recurring_deploy()
         timer:start(
             math.random(min_interval, max_interval),
